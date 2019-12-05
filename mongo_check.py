e011b89da3f4834561f19558f1de77aca0043190
Robert Carlos <robert.n.roll@gmail.com>
2019-12-05 11:33:38 -0300

    Módulo check corrigido.

--- mongo_check.py
+++ mongo_check.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 
 from packs.conversor import get_conversor
 from packs.insert import get_insert
+from packs.check import get_check
 
 
 class Connect(object):
@@ -14,35 +15,10 @@ class Connect(object):
             print("Could not connect to MongoDB")
 
 
-def get_check(dict_users):
-    client = Connect.get_connection()
-    db = client.admin
-    listing = db.command('usersInfo')
-    lista_users = [document['user'] for document in listing['users']]
-
-    try:
-        try:
-            custom_data = [get_conversor(document['customData']['ga'])
-                           for document in listing['users']]
-        except KeyError:
-            custom_data = [get_conversor(document['customData']['ars'])
-                           for document in listing['users']]
-    except Exception:
-        custom_data = 'empty'
-
-    print(custom_data)
-    for user, ga in dict_users.items():
-        if user in lista_users:
-            if custom_data is not 'empty' and ga in custom_data:
-                print(f'Nada a ser feito para o usuário {user}')
-            else:
-                print(f'Custom Data não encontrada no documento do {user}')
-                get_insert(db, user, ga)
-        else:
-            print(f'O usuário {user} não consta neste db')
-
-
 if __name__ == "__main__":
+
+    client = Connect.get_connection()
     dict_users = {"mongoadmin": 1713528, "shbd6001": 1579903,
                   "mongodb_exporter": 1713479, "smom6001": ""}
-    get_check(dict_users)
+
+    get_check(client, dict_users, get_conversor, get_insert)
