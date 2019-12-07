3f4e3672da1ed7e731aa5f68cc24a0ea6b58bf94
Robert Carlos <robert.n.roll@gmail.com>
2019-12-07 11:02:23 -0300

    Corrigido falha que no documento customData.

--- packs/check.py
+++ packs/check.py
@@ -4,20 +4,19 @@ def get_check(client, dict_users, func_get_conversor, func_get_insert):
 
     db = client.admin
     listing = db.command('usersInfo')
-    lista_users = [document['user'] for document in listing['users']]
+    lista_users_db = [document['user'] for document in listing['users']]
 
-    try:
-        try:
-            custom_data = [func_get_conversor(document['customData']['ga'])
-                           for document in listing['users']]
-        except KeyError:
-            custom_data = [func_get_conversor(document['customData']['ars'])
-                           for document in listing['users']]
-    except Exception:
-        custom_data = 'empty'
- 
     for user, ga in dict_users.items():
-        if user in lista_users:
+        if user in lista_users_db:
+            try:
+                try:
+                    custom_data = [func_get_conversor(doc['customData']['ga'])
+                                   for doc in listing['users'] if user in doc['user']]
+                except KeyError:
+                    custom_data = [func_get_conversor(doc['customData']['ars'])
+                                   for doc in listing['users'] if user in doc['user']]
+            except Exception:
+                custom_data = 'empty'
             if custom_data is not 'empty' and ga in custom_data:
                 print(f'Nada a ser feito para o usuário {user}')
             else:
